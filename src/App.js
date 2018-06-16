037e51fda9d705201b0ca80e120181d2681880a9
Tahara <tahara@studiom-web.net>
2018-06-16 23:08:31 +0900

    sass環境構築

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name = {
+  firstName: 'Masaharu',
+  lastName:'Tahara '
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">こんにちは {name.lastName + name.firstName}さん。</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
