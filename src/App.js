da259b3e24e8bc06b398bedbc78f2d9139a69d7a
Tahara <tahara@studiom-web.net>
2018-06-17 11:49:45 +0900

    add props and state to Class component
    Class componentにpropsとstateを追加

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+		super(props)
+		this.state = {name:"Haidoro"}
+	}
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello!</h1>
+          <h1>Hello!{this.state.name}</h1>
           <p>functionalコンポーネントを作成した</p>
         </header>
         <p className="App-intro">
